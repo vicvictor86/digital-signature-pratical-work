00a6797c6bc6d8fc4817c422d4e9f68d86015d6d
Amarildo Junior <ajunior283@outlook.com>
2023-01-21 15:35:22 -0300

    Adding menus and user interactions

--- main.py
+++ main.py
@@ -1,21 +1,76 @@
+import os
 from KeysOperations import generate_private_key
 from FilesOperations import readBase64File, readPlainTextFile, savePlainTextFile
 from SignaturesOperations import assign, verify
 from FilesOperations import saveFileInBase64
 
-textToTest = "first"
+option = 0
+sha = {224: "SHA224", 256: "SHA256", 384: "SHA384", 512: "SHA512"}
+while(True):
+    print("1. Assinar um arquivo de texto")
+    print("2. Verificar assinatura de um arquivo de texto")
+    print("0. Sair")
+    option = int(input("Digite a opção desejada: "))
 
-savePlainTextFile('text', textToTest)
+    sha_version = 0
 
-private_key = generate_private_key()
+    if(option == 1):
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-public_key, signature = assign(textToTest, private_key, "SHA224")
+        textToTest = str(input("Digite o texto a ser assinado: "))
 
-saveFileInBase64('my_publickey', public_key)
-saveFileInBase64('signature', signature)
+        savePlainTextFile(str(input("Digite o nome do arquivo txt a ser salvo: ")), textToTest)
 
-base64_public_key = readBase64File('my_publickey')
-base64_signature = readBase64File('signature')
-text = readPlainTextFile('text')
+        private_key = generate_private_key()
+        
+        while(True):
+            print("Versões disponíveis: \nSHA-224\nSHA-256\nSHA-384\nSHA-512")
+            sha_version = int(input("Digite o numero correspondente a versão desejada do SHA: "))
+            if sha_version in sha.keys():
+                public_key, signature = assign(textToTest, private_key, sha[sha_version])
+                break
+            else:
+                print("Opção inválida! Digite novamente!")
 
-print(verify(text, base64_public_key, base64_signature, "SHA224"))
+        saveFileInBase64(str(input("Digite o nome do arquivo que vai conter a chave pública: ")), public_key)
+        saveFileInBase64(str(input("Digite o nome do arquivo que vai conter a mensagem assinada: ")), signature)
+
+        print("Assinatura realizada!")
+    elif(option == 2):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        base64_public_key, base64_signature, text = "", "", ""
+        openFiles = False
+
+        try : 
+            base64_public_key = readBase64File(str(input("Digite o nome do arquivo que contém a chave: ")))
+            base64_signature = readBase64File(str(input("Digite o nome do arquivo que contém a mensagem assinada: ")))
+            text = readPlainTextFile(str(input("Digite o nome do arquivo txt de entrada: ")))
+            openFiles = True
+        except:
+            print("Nome de arquivo inválido!")
+
+        if openFiles:
+            while(True):
+                print("Versões disponíveis: \nSHA-224\nSHA-256\nSHA-384\nSHA-512")
+                sha_version = int(input("Digite o numero correspondente a versão desejada do SHA: "))
+                if sha_version in sha.keys():
+                    break
+                else:
+                    print("Opção inválida! Digite novamente!")
+
+            if verify(text, base64_public_key, base64_signature, sha[sha_version]):                                          
+                print("Assinatura válida!")
+            else:
+                print("Assinatura inválida!")
+        
+        else:
+            
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("Erro ao abrir arquivos!")
+            
+            
+    elif(option == 0):
+        print("Finalizando...")
+        break
+
