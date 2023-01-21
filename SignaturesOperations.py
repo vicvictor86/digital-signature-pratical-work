1fbcb57b93442b68f2b7dde84ab297fc99dfbd4b
Victor Emmanuel <vicbem@hotmail.com>
2023-01-21 00:29:23 -0300

    Isolate Signature operations, main now has only input ant output functions

--- SignaturesOperations.py
+++ SignaturesOperations.py
@@ -1,6 +1,10 @@
 from Crypto.Signature import pkcs1_15
 from Crypto.PublicKey import RSA
 
+from HashOperations import generate_hash
+from KeysOperations import get_public_key
+from base64 import b64encode, b64decode
+
 def generate_signature(private_key, hash_object):
   return pkcs1_15.new(private_key).sign(hash_object)
 
@@ -8,6 +12,34 @@ def verify_signature(public_key, signature, hash_object):
   try: 
     key = RSA.import_key(public_key)
     pkcs1_15.new(key).verify(hash_object, signature)
+    
     return True
   except (ValueError, TypeError):
     return False
+
+def assign(plain_text, private_key, hash_algorithm_name):
+  hash_object = generate_hash(plain_text, hash_algorithm_name)
+
+  if hash_object is None:
+    raise ValueError("Hash algorithm not supported")
+
+  hash_hexadecimal = hash_object.hexdigest().encode('utf-8')
+  hash_of_hash_hexadecimal = generate_hash(hash_hexadecimal, hash_algorithm_name)
+
+  public_key = get_public_key(private_key)
+  base64_public_key = b64encode(public_key)
+
+  signature = generate_signature(private_key, hash_of_hash_hexadecimal)
+  base64_signature = b64encode(signature)
+  
+  return base64_public_key, base64_signature
+
+def verify(plain_text, base64_public_key, base64_signature, hash_algorithm_name):
+  public_key = b64decode(base64_public_key)
+  signature = b64decode(base64_signature)
+
+  read_hash_object = generate_hash(plain_text, hash_algorithm_name)
+  hash_hexadecimal = read_hash_object.hexdigest().encode('utf-8')
+  hash_of_hash_hexadecimal = generate_hash(hash_hexadecimal, hash_algorithm_name)
+
+  return verify_signature(public_key, signature, hash_of_hash_hexadecimal)
--- main.py
+++ main.py
@@ -1,41 +1,7 @@
-from base64 import b64encode, b64decode
-
-from KeysOperations import generate_private_key, get_public_key
-from SignaturesOperations import generate_signature, verify_signature
-from HashOperations import generate_hash
-from FilesOperations import saveFileInBase64, readBase64File, readPlainTextFile, savePlainTextFile
-
-def assign(plain_text, private_key, hash_algorithm_name):
-  hash_object = generate_hash(plain_text, hash_algorithm_name)
-
-  if hash_object is None:
-    raise ValueError("Hash algorithm not supported")
-
-  # If it is really necessary to encrypt the hash, uncomment the following lines
-  hash_hexadecimal = hash_object.hexdigest().encode('utf-8')
-  hash_of_hash_hexadecimal = generate_hash(hash_hexadecimal, hash_algorithm_name)
-
-  public_key = get_public_key(private_key)
-  base64_public_key = b64encode(public_key)
-
-  saveFileInBase64('my_publickey', base64_public_key)
-
-  signature = generate_signature(private_key, hash_of_hash_hexadecimal)
-  base64_signature = b64encode(signature)
-
-  saveFileInBase64('signature', base64_signature)
-  
-  return base64_public_key, base64_signature
-
-def verify(plain_text, base64_public_key, base64_signature, hash_algorithm_name):
-  public_key = b64decode(base64_public_key)
-  signature = b64decode(base64_signature)
-
-  read_hash_object = generate_hash(plain_text, hash_algorithm_name)
-  hash_hexadecimal = read_hash_object.hexdigest().encode('utf-8')
-  hash_of_hash_hexadecimal = generate_hash(hash_hexadecimal, hash_algorithm_name)
-
-  return verify_signature(public_key, signature, hash_of_hash_hexadecimal)
+from KeysOperations import generate_private_key
+from FilesOperations import readBase64File, readPlainTextFile, savePlainTextFile
+from SignaturesOperations import assign, verify
+from FilesOperations import saveFileInBase64
 
 textToTest = "first"
 
@@ -44,6 +10,9 @@ savePlainTextFile('text', textToTest)
 private_key = generate_private_key()
 
 public_key, signature = assign(textToTest, private_key, "SHA224")
+
+saveFileInBase64('my_publickey', public_key)
+saveFileInBase64('signature', signature)
 
 base64_public_key = readBase64File('my_publickey')
 base64_signature = readBase64File('signature')
